a32409a83326f4f7859358d0222ffbe7513a9c32
laukhin-alex <alex.laukhin@yandex.ru>
2022-02-22 21:46:08 +0300

    HW1 is done

--- Netology_IB_Instruments/ProfileView.swift
+++ Netology_IB_Instruments/ProfileView.swift
@@ -5,4 +5,31 @@
 //  Created by Александр on 22.02.2022.
 //
 
-import Foundation
+import UIKit
+
+class ProfileView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setupView()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.setupView()
+    }
+
+    private func setupView() {
+        let view = self.loadViewFromXib()
+        self.addSubview(view)
+        view.frame = self.bounds
+
+    }
+
+    private func loadViewFromXib() -> UIView {
+        guard let view = Bundle.main.loadNibNamed("ProfileView", owner: self, options: nil)?.first as? UIView else { return UIView() }
+
+        return view
+    }
+
+}
---
+++ Netology_IB_Instruments/ProfileViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by Александр on 22.02.2022.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .blue
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+
+}
